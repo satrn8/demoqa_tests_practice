362ee509cdcd92a97912261f0595aae74fc55e02
satrn8 <verypyc@gmail.com>
2022-07-13 00:51:04 +0300

    second commit

--- test_pracrice_form/version_2.py
+++ test_pracrice_form/version_2.py
@@ -9,9 +9,9 @@ class Student:
     email = "verypyc@gmail.com"
     gender = "Female"
     mobile = "9998889988"
-    date_of_birth = {"day": "027", "month": "06", "year": "1992"}
+    date_of_birth = "27 July,1992"
     subjects = "Maths"
-    hobbies = "Sports"
+    hobbies = "Reading"
     picture = "pepe.png"
     address = "Moscow"
     state = "NCR"
@@ -38,14 +38,14 @@ def test_registration_form():
     browser.element("#submit").press_enter()
 
     browser.element('#example-modal-sizes-title-lg').should(have.exact_text('Thanks for submitting the form'))
-    browser.all(".modal-dialog").all("table tr")[1].all("td").should(have.exact_texts("Student Name", "Alyona Tch"))
-    browser.all(".modal-dialog").all("table tr")[2].all("td").should(have.exact_texts("Student Email", "verypyc@gmail.com"))
-    browser.all(".modal-dialog").all("table tr")[3].all("td").should(have.exact_texts("Gender", "Female"))
-    browser.all(".modal-dialog").all("table tr")[4].all("td").should(have.exact_texts("Mobile", "9998889988"))
-    browser.all(".modal-dialog").all("table tr")[5].all("td").should(have.exact_texts("Date of Birth", "27 July,1992"))
-    browser.all(".modal-dialog").all("table tr")[6].all("td").should(have.exact_texts("Subjects", "Maths"))
-    browser.all(".modal-dialog").all("table tr")[7].all("td").should(have.exact_texts("Hobbies", "Reading"))
-    browser.all(".modal-dialog").all("table tr")[8].all("td").should(have.exact_texts("Picture", 'pepe.png'))
-    browser.all(".modal-dialog").all("table tr")[9].all("td").should(have.exact_texts("Address", "Moscow"))
-    browser.all(".modal-dialog").all("table tr")[10].all("td").should(have.exact_texts("State and City", "NCR Gurgaon"))
+    browser.all(".modal-dialog").all("table tr")[1].all("td").should(have.exact_texts("Student Name", f"{Student.first_name} {Student.last_name}"))
+    browser.all(".modal-dialog").all("table tr")[2].all("td").should(have.exact_texts("Student Email", Student.email))
+    browser.all(".modal-dialog").all("table tr")[3].all("td").should(have.exact_texts("Gender", Student.gender))
+    browser.all(".modal-dialog").all("table tr")[4].all("td").should(have.exact_texts("Mobile", Student.mobile))
+    browser.all(".modal-dialog").all("table tr")[5].all("td").should(have.exact_texts("Date of Birth", Student.date_of_birth))
+    browser.all(".modal-dialog").all("table tr")[6].all("td").should(have.exact_texts("Subjects", Student.subjects))
+    browser.all(".modal-dialog").all("table tr")[7].all("td").should(have.exact_texts("Hobbies", Student.hobbies))
+    browser.all(".modal-dialog").all("table tr")[8].all("td").should(have.exact_texts("Picture", Student.picture))
+    browser.all(".modal-dialog").all("table tr")[9].all("td").should(have.exact_texts("Address", Student.address))
+    browser.all(".modal-dialog").all("table tr")[10].all("td").should(have.exact_texts("State and City", f"{Student.state} {Student.city}"))
     browser.element("#closeLargeModal").press_enter()
